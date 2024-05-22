7431d102ef9dad775f41b605b94afc8bd8806405
kau16154 <kau16154@sheridancollege.ca>
2024-05-21 20:55:06 -0400

    version1

---
+++ src/studentdetails/Student.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdetails;
+
+/**
+ *
+ * @author simra
+ */
+public class Student {
+    private String name;
+     private int age;
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
